685e88f2952547fbb7fad7854467b77f1d73170b
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-08-23 22:37:18 +0300

    RxJava

---
+++ src/main/java/ua/in/sz/rxjava/RxMain.java
@@ -0,0 +1,52 @@
+package ua.in.sz.rxjava;
+
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+
+import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class RxMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Start");
+
+        Observable<String> observable = Observable.fromIterable(RxMain::items)
+                .subscribeOn(scheduler("subscribe-%d"))
+                .observeOn(scheduler("observe-%d"));
+
+        Disposable subscribe = observable
+                .subscribe(e -> log.info("Receive: {}", e));
+
+        while (!subscribe.isDisposed()) {
+            log.info("Wait");
+            Thread.sleep(10);
+        }
+
+        log.info("End");
+    }
+
+    private static Iterator<String> items() {
+        return Stream.iterate(0, i -> i + 1)
+                .map(i -> "Event-" + i)
+                .limit(4)
+                .peek(e -> log.info("Send: {}", e))
+                .iterator();
+    }
+
+    private static Scheduler scheduler(String prefix) {
+        BasicThreadFactory factory = new BasicThreadFactory.Builder()
+                .namingPattern(prefix)
+                .daemon(true)
+                .build();
+
+        return Schedulers.from(
+                Executors.newFixedThreadPool(4, factory));
+    }
+}
