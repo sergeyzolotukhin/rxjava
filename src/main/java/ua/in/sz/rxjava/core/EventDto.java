07c2842fbb7e2e53b1598f5227c4639a5599b39c
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-08-24 12:41:46 +0300

    RxJava flat map and event listing

--- src/main/java/ua/in/sz/rxjava/core/EventDto.java
+++ src/main/java/ua/in/sz/rxjava/core/EventDto.java
@@ -3,13 +3,12 @@ package ua.in.sz.rxjava.core;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @ToString
 @RequiredArgsConstructor(staticName = "of")
-public class EventDto {
+class EventDto {
     private final String name;
     private final int no;
 }
---
+++ src/main/java/ua/in/sz/rxjava/core/FlatMap.java
@@ -0,0 +1,30 @@
+package ua.in.sz.rxjava.core;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+@Slf4j
+public class FlatMap {
+    public static void main(String[] args) {
+        Observable<String> observable = Observable.fromIterable(FlatMap::numbers)
+                .flatMap(FlatMap::children);
+
+        Disposable disposable = observable.subscribe(i -> log.info("Receive {}", i));
+    }
+
+    private static ObservableSource<String> children(String p) {
+        return Observable.just("" + p + "-1", "" + p + "-2");
+    }
+
+    private static Iterator<String> numbers() {
+        return Stream.iterate(0, i -> i + 1)
+                .limit(10)
+                .map(i -> "" + i)
+                .iterator();
+    }
+}
