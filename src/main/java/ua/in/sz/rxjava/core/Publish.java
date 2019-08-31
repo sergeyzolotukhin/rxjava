4681b2e2a27774951336760a2e740490eef5ffa5
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-09-01 01:19:39 +0300

    RxJava publish command

--- src/main/java/ua/in/sz/rxjava/core/Publish.java
+++ src/main/java/ua/in/sz/rxjava/core/Publish.java
@@ -1,11 +1,17 @@
 package ua.in.sz.rxjava.core;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 import java.util.Iterator;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -13,8 +19,13 @@ public class Publish {
     public static void main(String[] args) {
         ConnectableObservable<String> observable = Observable.fromIterable(Publish::items).publish();
 
+//        Observable<String> observable1 = observable.subscribeOn(scheduler("subscribe-%d"));
+//        Observable<String> observable2 = observable.observeOn(scheduler("observe-%d"));
+
         Disposable disposable1 = observable.subscribe(e -> log.info("Receive 1: {}", e));
         Disposable disposable2 = observable.subscribe(e -> log.info("Receive 2: {}", e));
+
+//        Flowable<String> flowable = observable.toFlowable(BackpressureStrategy.MISSING);
 
         log.info("Subscribed");
 
@@ -29,5 +40,15 @@ public class Publish {
                 .limit(4)
                 .peek(e -> log.info("Send: {}", e))
                 .iterator();
+    }
+
+    private static Scheduler scheduler(String prefix) {
+        BasicThreadFactory factory = new BasicThreadFactory.Builder()
+                .namingPattern(prefix)
+                .daemon(true)
+                .build();
+
+        return Schedulers.from(
+                Executors.newFixedThreadPool(4, factory));
     }
 }
