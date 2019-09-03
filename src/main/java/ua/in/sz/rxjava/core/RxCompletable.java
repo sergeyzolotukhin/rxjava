6f13a0faa600ea429a6046f4971e308141704f10
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-09-04 00:42:41 +0300

    RxJava publish command

---
+++ src/main/java/ua/in/sz/rxjava/core/RxCompletable.java
@@ -0,0 +1,23 @@
+package ua.in.sz.rxjava.core;
+
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class RxCompletable {
+    public static void main(String[] args) {
+        log.info("Start");
+
+        Observable<Long> observable = Observable.intervalRange(
+                0, 10, 0, 500, TimeUnit.MILLISECONDS);
+
+        observable.subscribe((i) -> log.info("Receive: {}", i));
+        Completable.fromObservable(observable).blockingAwait();
+
+        log.info("End");
+    }
+}
