09a79509a423458497de3777e2432b59d3a35cc4
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-09-03 23:37:10 +0300

    RxJava publish command

---
+++ src/main/java/ua/in/sz/rxjava/core/RxDelay.java
@@ -0,0 +1,22 @@
+package ua.in.sz.rxjava.core;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class RxDelay {
+    public static void main(String[] args) {
+        log.info("Start");
+
+        Disposable disposable = Observable.just(1, 2, 4)
+                .delay(5, TimeUnit.SECONDS)
+                .subscribe((i) -> log.info("Receive: {}", i));
+
+        WaitUtils.wait(disposable);
+
+        log.info("End");
+    }
+}
